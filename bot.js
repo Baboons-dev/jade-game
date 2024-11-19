79abfb99ecac6db3812adffe4926b36449cc7e63
laibawaqar <laibawaqark@gmail.com>
2024-11-19 04:20:12 -0500

    bot.js update

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require('telegraf');
 const crypto = require('crypto-js');
 
-const web_link = 'https://june-tg-bot.baboons.tech/';
-const TOKEN = '7992709613:AAHI5IP6VFGeuxpuKGZ_xVaN-rWQv7gsRXE';
+const web_link = 'https://jade.baboons.tech/';
+const TOKEN = '7914604069:AAFNrVN4BGxeXcjhonTz-uRQqM79n5xk2QM';
 
 const bot = new Telegraf(TOKEN);
 var secretKey = '02PhgD8F4bgtA0T8WB2/y0dvrtHKXXMwtht3HZgyHSzAUrJwXRYjUV9z0lLB3ass';
@@ -47,28 +47,28 @@ bot.command('start', async (ctx) => {
   } else {
     web_final_link = web_link + '?tgId=' + encodeURIComponent(encrypted);
   }
-  ctx.replyWithPhoto(
-    {
-      source: 'tgcover.jpg',
-    },
-    {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: '🕹 Play & Earn',
-              web_app: {
-                url: web_final_link,
-              },
-            },
-          ],
-          [{ text: '🔊 Telegram COMMUNITY', url: 'https://t.me/crypto_june' }],
-          [{ text: '🫂 FOLLOW X', url: 'https://x.com/crypto_june' }],
-        ],
-      },
-      caption: 'Start snow in june ' + '\n' + "Let's tap-tap snow and win!",
-    }
-  );
+  // ctx.replyWithPhoto(
+  //   {
+  //     source: 'tgcover.jpg',
+  //   },
+  //   {
+  //     reply_markup: {
+  //       inline_keyboard: [
+  //         [
+  //           {
+  //             text: '🕹 Play & Earn',
+  //             web_app: {
+  //               url: web_final_link,
+  //             },
+  //           },
+  //         ],
+  //         [{ text: '🔊 Telegram COMMUNITY', url: 'https://t.me/crypto_june' }],
+  //         [{ text: '🫂 FOLLOW X', url: 'https://x.com/crypto_june' }],
+  //       ],
+  //     },
+  //     caption: 'Start snow in june ' + '\n' + "Let's tap-tap snow and win!",
+  //   }
+  // );
   ctx.setChatMenuButton({
     text: 'Game',
     type: 'web_app',
