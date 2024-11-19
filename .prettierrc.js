a73e27de326e2e2fa97df102e564e49ce73d825a
laibawaqar <laibawaqark@gmail.com>
2024-11-18 23:13:49 -0500

    remove env, add eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  semi: true,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
